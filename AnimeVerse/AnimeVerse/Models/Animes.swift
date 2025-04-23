6f7bfb7c9a0f828c771514459fea4fa340af9c53
luannluna <luann.marques@gmail.com>
2025-04-23 20:23:01 -0300

    added Animes Model

--- AnimeVerse/AnimeVerse/Models/Animes.swift
+++ AnimeVerse/AnimeVerse/Models/Animes.swift
@@ -1,6 +1,10 @@
-struct Anime: Identifiable {
+import Foundation
+
+struct Anime: Identifiable, Equatable {
     let id: Int
-    let title: String
-    let siteUrl: String?
-    let description: String?
+    let titleRomaji: String
+    let titleEnglish: String?
+    let titleNative: String
+    let startDate: DateComponents
+    let coverImageURL: URL
 }
