68230c88971457a468284da6381d2b82338921ca
luannluna <luann.marques@gmail.com>
2025-04-25 14:30:56 -0300

    fixed extension

--- AnimeVerse/AnimeVerse/Extensions/URL+Extensions.swift
+++ AnimeVerse/AnimeVerse/Extensions/URL+Extensions.swift
@@ -42,7 +42,8 @@ extension URL {
     func appendingQueryParameters(_ parameters: [String: String]) -> URL? {
         var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
         let queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
-        components?.queryItems = (components?.queryItems ?? []) + queryItems
+        let componentsQueryItems = components?.queryItems ?? []
+        components?.queryItems = componentsQueryItems + queryItems
         return components?.url
     }
     
